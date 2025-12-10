7c4d129be3bd321dd1bdd59f8be6f3d96801a8af
parkrms <131834850+parkrms@users.noreply.github.com>
2025-12-11 03:36:35 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -103,7 +103,7 @@ function calculateCardDimensions() {
 // 3. 메인 렌더링 루프 (Draw)
 // ==========================================
 function draw() {
-  background(40); 
+  background(20); 
   lights(); 
   
   for (let i = 0; i < cards.length; i++) {
